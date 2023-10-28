cf3ebb7d50098f659bedee07d744317974503667
mr-Bones3108 <nikhil788p@gmail.com>
2023-10-28 09:10:10 +0530

    add navbar and card styling

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ function Book(title, author, pages, read = false) {
 const Book1 = new Book("A Passage to India", "E.M. Forster", 234, true);
 const Book2 = new Book("Far from the Madding Crowd","Thomas Hardy", 321, false)
 const Book3 = new Book("The Merchant of Venice","Shakespeare", 411, true)
+const Book4 = new Book("The Merchant of Venice","Shakespeare", 411, true)
 myLibrary.push(Book1);
 myLibrary.push(Book2);
 myLibrary.push(Book3);
+myLibrary.push(Book4)
 
 
 function displayBook(Book){
@@ -20,15 +22,25 @@ function displayBook(Book){
     bookCard.classList = 'card'
 
     var bookTitle = document.createElement('p')
+    bookTitle.classList='title-heading'
     bookTitle.textContent=`${Book.title}`
 
     var bookAuthor = document.createElement('p')
-    bookAuthor.textContent=`by ${Book.author}`
+    bookAuthor.classList='author-heading'
+    bookAuthor.textContent=`- by ${Book.author}`
+
+    var bookPages = document.createElement('p')
+    bookPages.textContent = `${Book.pages} pages`
 
     var cardBody = document.getElementById("cardContainer")
     bookCard.appendChild(bookTitle)
     bookCard.appendChild(bookAuthor)
+    bookCard.appendChild(bookPages)
     cardBody.appendChild(bookCard)
+}
+
+function addBookToLibrary() {
+
 }
 
 myLibrary.forEach(Book => {
